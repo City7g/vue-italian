c7a9c941c9ad2d77ac1c4a1d29518a9507755116
Дима Мироненко <city7gor@gmail.com>
2021-08-22 14:41:54 +0300

    Fixed import swiper

--- src/components/MainSlider.vue
+++ src/components/MainSlider.vue
@@ -16,9 +16,6 @@
 <script>
 import SwiperCore, { Navigation } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/vue";
-import "swiper/swiper.scss";
-import "swiper/components/navigation/navigation.scss";
-// import "swiper/components/pagination/pagination.scss";
 
 SwiperCore.use([Navigation]);
 
@@ -29,84 +26,86 @@ export default {
 </script>
 
 <style lang="scss">
-// .c-slider {
-//   &__item {
-//     padding: 60px 30px;
+@import "swiper/swiper.scss";
+@import "swiper/components/navigation/navigation.scss";
+.c-slider {
+  &__item {
+    padding: 60px 30px;
 
-//     background-color: $grey-bg;
+    background-color: $grey-bg;
 
-//     &:nth-child(2n) {
-//       background-color: $green;
-//     }
+    &:nth-child(2n) {
+      background-color: $green;
+    }
 
-//     @media (max-width: 992px) {
-//       padding: 30px 30px;
-//     }
+    @media (max-width: 992px) {
+      padding: 30px 30px;
+    }
 
-//     & img {
-//       margin: 0 auto;
-//     }
-//   }
+    & img {
+      margin: 0 auto;
+    }
+  }
 
-//   & .swiper-button-prev,
-//   & .swiper-button-next {
-//     position: absolute;
-//     left: auto;
-//     top: auto;
-//     bottom: 0;
+  & .swiper-button-prev,
+  & .swiper-button-next {
+    position: absolute;
+    left: auto;
+    top: auto;
+    bottom: 0;
 
-//     width: 60px;
-//     height: 70px;
+    width: 60px;
+    height: 70px;
 
-//     color: $white;
-//     background-color: $green;
-//     border: 1px solid $black;
-//     transition: $transition;
-//     z-index: 3;
+    color: $white;
+    background-color: $green;
+    border: 1px solid $black;
+    transition: $transition;
+    z-index: 3;
 
-//     @media (max-width: 768px) {
-//       width: 30px;
-//       height: 40px;
-//     }
+    @media (max-width: 768px) {
+      width: 30px;
+      height: 40px;
+    }
 
-//     &::after {
-//       font-size: 25px;
+    &::after {
+      font-size: 25px;
 
-//       @media (max-width: 768px) {
-//         font-size: 15px;
-//       }
-//     }
-//   }
+      @media (max-width: 768px) {
+        font-size: 15px;
+      }
+    }
+  }
 
-//   & .swiper-button-prev {
-//     right: 59px;
+  & .swiper-button-prev {
+    right: 59px;
 
-//     @media (max-width: 768px) {
-//       right: 29px;
-//     }
-//   }
+    @media (max-width: 768px) {
+      right: 29px;
+    }
+  }
 
-//   & .swiper-button-next {
-//     right: 0;
-//   }
-//   & .swiper-button-disabled {
-//     opacity: 1;
-//     color: $black;
-//     background-color: $white;
-//   }
-// }
-// .slider__item {
-//   flex: 0 0 50%;
+  & .swiper-button-next {
+    right: 0;
+  }
+  & .swiper-button-disabled {
+    opacity: 1;
+    color: $black;
+    background-color: $white;
+  }
+}
+.slider__item {
+  flex: 0 0 50%;
 
-//   padding: 30px;
+  padding: 30px;
 
-//   background-color: $grey-bg;
+  background-color: $grey-bg;
 
-//   & > * {
-//     // margin: 0 auto;
-//   }
-// }
-// .slider__item:nth-child(2n) {
-//   background-color: $green;
-// }
+  & > * {
+    // margin: 0 auto;
+  }
+}
+.slider__item:nth-child(2n) {
+  background-color: $green;
+}
 </style>
