1719899ce63438300f811b91c7d303eb0ee7f897
Дима Мироненко <city7gor@gmail.com>
2021-08-22 14:35:38 +0300

    Add z-index

--- src/components/MainSlider.vue
+++ src/components/MainSlider.vue
@@ -29,72 +29,72 @@ export default {
 </script>
 
 <style lang="scss">
-.c-slider {
-  &__item {
-    padding: 60px 30px;
+// .c-slider {
+//   &__item {
+//     padding: 60px 30px;
 
-    background-color: $grey-bg;
+//     background-color: $grey-bg;
 
-    &:nth-child(2n) {
-      background-color: $green;
-    }
+//     &:nth-child(2n) {
+//       background-color: $green;
+//     }
 
-    @media (max-width: 992px) {
-      padding: 30px 30px;
-    }
+//     @media (max-width: 992px) {
+//       padding: 30px 30px;
+//     }
 
-    & img {
-      margin: 0 auto;
-    }
-  }
+//     & img {
+//       margin: 0 auto;
+//     }
+//   }
 
-  & .swiper-button-prev,
-  & .swiper-button-next {
-    position: absolute;
-    left: auto;
-    top: auto;
-    bottom: 0;
+//   & .swiper-button-prev,
+//   & .swiper-button-next {
+//     position: absolute;
+//     left: auto;
+//     top: auto;
+//     bottom: 0;
 
-    width: 60px;
-    height: 70px;
+//     width: 60px;
+//     height: 70px;
 
-    color: $white;
-    background-color: $green;
-    border: 1px solid $black;
-    transition: $transition;
-    z-index: 3;
+//     color: $white;
+//     background-color: $green;
+//     border: 1px solid $black;
+//     transition: $transition;
+//     z-index: 3;
 
-    @media (max-width: 768px) {
-      width: 30px;
-      height: 40px;
-    }
+//     @media (max-width: 768px) {
+//       width: 30px;
+//       height: 40px;
+//     }
 
-    &::after {
-      font-size: 25px;
+//     &::after {
+//       font-size: 25px;
 
-      @media (max-width: 768px) {
-        font-size: 15px;
-      }
-    }
-  }
+//       @media (max-width: 768px) {
+//         font-size: 15px;
+//       }
+//     }
+//   }
 
-  & .swiper-button-prev {
-    right: 59px;
+//   & .swiper-button-prev {
+//     right: 59px;
 
-    @media (max-width: 768px) {
-      right: 29px;
-    }
-  }
+//     @media (max-width: 768px) {
+//       right: 29px;
+//     }
+//   }
 
-  & .swiper-button-next {
-    right: 0;
-  }
-  & .swiper-button-disabled {
-    opacity: 1;
-    color: $black;
-    background-color: $white;
-  }
-}
+//   & .swiper-button-next {
+//     right: 0;
+//   }
+//   & .swiper-button-disabled {
+//     opacity: 1;
+//     color: $black;
+//     background-color: $white;
+//   }
+// }
 // .slider__item {
 //   flex: 0 0 50%;
 
